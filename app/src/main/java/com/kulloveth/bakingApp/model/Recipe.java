d0379af7c6d0ff17a9ba2d47ece91d5fa4320fe2
kulloveth <kulloveth@gmail.com>
2020-05-27 12:49:30 +0100

    setup retrofit instances

--- app/src/main/java/com/kulloveth/bakingApp/model/Recipe.java
+++ app/src/main/java/com/kulloveth/bakingApp/model/Recipe.java
@@ -1,11 +1,15 @@
 package com.kulloveth.bakingApp.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
+import java.util.Objects;
 
-public class Recipe {
+public class Recipe implements Parcelable {
 
     @SerializedName("id")
     @Expose
@@ -25,4 +29,102 @@ public class Recipe {
     @SerializedName("image")
     @Expose
     private String image;
+
+    public Recipe() {
+    }
+
+    public Recipe(int id, String name, List<Ingredient> ingredients, List<Step> steps, int servings, String image) {
+        this.id = id;
+        this.name = name;
+        this.ingredients = ingredients;
+        this.steps = steps;
+        this.servings = servings;
+        this.image = image;
+    }
+
+    protected Recipe(Parcel in) {
+        id = in.readInt();
+        name = in.readString();
+        servings = in.readInt();
+        image = in.readString();
+    }
+
+    public static final Creator<Recipe> CREATOR = new Creator<Recipe>() {
+        @Override
+        public Recipe createFromParcel(Parcel in) {
+            return new Recipe(in);
+        }
+
+        @Override
+        public Recipe[] newArray(int size) {
+            return new Recipe[size];
+        }
+    };
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public List<Step> getSteps() {
+        return steps;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Recipe)) return false;
+        Recipe recipe = (Recipe) o;
+        return getId() == recipe.getId() &&
+                getServings() == recipe.getServings() &&
+                getName().equals(recipe.getName()) &&
+                getIngredients().equals(recipe.getIngredients()) &&
+                getSteps().equals(recipe.getSteps()) &&
+                getImage().equals(recipe.getImage());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getIngredients(), getSteps(), getServings(), getImage());
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", ingredients=" + ingredients +
+                ", steps=" + steps +
+                ", servings=" + servings +
+                ", image='" + image + '\'' +
+                '}';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(name);
+        dest.writeInt(servings);
+        dest.writeString(image);
+    }
 }
--- app/src/main/java/com/kulloveth/bakingApp/utils/ApiUtils.java
+++ app/src/main/java/com/kulloveth/bakingApp/utils/ApiUtils.java
@@ -1,7 +1,13 @@
 package com.kulloveth.bakingApp.utils;
 
+import com.kulloveth.bakingApp.retrofit.BakingApiServiceInterface;
+import com.kulloveth.bakingApp.retrofit.RetrofitClient;
+
 public class ApiUtils {
 
     private static final String BASE_API_URL = "https://d17h27t6h515a5.cloudfront.net/";
 
+    public static BakingApiServiceInterface getBakingApiServiceInterface() {
+        return RetrofitClient.getRetrofitClient(BASE_API_URL).create(BakingApiServiceInterface.class);
+    }
 }
